0add306fd89e47657c7096d4ef072e60a83f104f
Jacob Hooey <jacob@pompouspanda.com>
2013-11-08 01:25:25 -0500

    pimped the admin out by adding a custom PostAdmin class to improve the UX a little bit

--- blog/admin.py
+++ blog/admin.py
@@ -3,4 +3,17 @@ from django.contrib import admin
 # Register your models here.
 from blog.models import Post
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    # fields display on change list
+    list_display = ['title', 'description']
+    # fields to filter the change list with
+    list_filter = ['published', 'created']
+    # fields to search in change list
+    search_fields = ['title', 'description', 'content']
+    # enable the save buttons on top on change form
+    save_on_top = True
+    # prepopulate the slug from the title - big timesaver!
+    prepopulated_fields = {"slug": ("title",)}
+    
+
+admin.site.register(Post, PostAdmin)
