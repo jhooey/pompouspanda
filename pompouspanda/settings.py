c956d6d9f13548c05cff816e35d1b1fa53e5d96c
Jacob Hooey <jacob@pompouspanda.com>
2013-11-09 17:39:40 -0500

    added view templates

--- pompouspanda/settings.py
+++ pompouspanda/settings.py
@@ -82,3 +82,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/STATIC/'
+
+TEMPLATE_DIRS = ('/home/jhooey/workspace/pompouspanda/pompouspanda/templates',)
