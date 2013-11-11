9d39c3c6b79ab3672989b37d4d32682cc950ff13
Jacob Hooey <jacob@pompouspanda.com>
2013-11-11 02:24:28 -0500

    fixed the urls

--- pompouspanda/urls.py
+++ pompouspanda/urls.py
@@ -2,28 +2,15 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 admin.autodiscover()
-
-(r'^$', 'pompouspanda.blog.views.index'),
-url(
-    r'^blog/view/(?P<slug>[^\.]+).html', 
-    'pompouspanda.blog.views.view_post', 
-    name='view_blog_post'),
-url(
-    r'^blog/category/(?P<slug>[^\.]+).html', 
-    'pompouspanda.blog.views.view_category', 
-    name='view_blog_category'),
-
-
-
-
-
-
-#urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'pompouspanda.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-#    url(r'^admin/', include(admin.site.urls)),
-#    url(r'^$', 'blog.views.index'),
-#    url(r'^(?P<slug>[\w\-]+)/$', 'blog.views.post'),
-#)
+urlpatterns = patterns('',
+                        url(r'^$', 'blog.views.index', name='home'),
+                        url(r'^admin/', include(admin.site.urls)),
+                        url(
+                            r'^view/(?P<slug>[^\.]+).html', 
+                            'blog.views.view_post', 
+                            name='view_blog_post'),
+                        url(
+                            r'^category/(?P<slug>[^\.]+).html', 
+                            'blog.views.view_category', 
+                            name='view_blog_category'),
+)
